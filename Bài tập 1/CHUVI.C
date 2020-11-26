fe27131486106a6868e3f0411faee5c030d185dd
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2020-11-26 16:47:20 +0700

    Create CHUVI.C

---
+++ Bài tập 1/CHUVI.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+ int main(){
+// khai bao bien R,PI,CV,DT kieu double
+ double R,PI,CV,DT;
+ float chuvi, dientich;
+
+ // gan gia tri bien
+ 
+ R = 2.5;
+ PI = 3.14;
+ chuvi = 2*R*PI;
+ dientich = R*R*PI;
+ // in ra man hinh
+ printf("chu vi \ t dien tich \n %0.1f\ t %1f",chuvi,dientich);
+ getchar();
+ return 0;
+ }
