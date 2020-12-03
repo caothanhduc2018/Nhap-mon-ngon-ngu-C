db905e5f1794ceefda7a97c2bb4584872dcc34d8
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2020-12-03 15:15:41 +0700

    Create chieudai.c

---
+++ Bài tập 2/chieudai.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int mian(void){
+
+int l;
+
+double m,cm,mm ;
+printf ("Nhap chieu dai  l=");
+scanf ("%d",&l);
+printf ("nhap cm =");
+scanf ("%d", & cm);
+printf ("nhap mm =");
+scanf ("%d", & mm);
+ m=l;
+cm=l*100;
+mm=l*1000;
+print("m\t cm\t mm\t %d %d %d",m,cm,mm);
+return 0;
+}
