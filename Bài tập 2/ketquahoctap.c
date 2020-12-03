8cb58e1ff9be6a3ec01fed68ac152eb012107cac
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2020-12-03 16:47:22 +0700

    Update ketquahoctap.c

--- Bài tập 2/ketquahoctap.c
+++ Bài tập 2/ketquahoctap.c
@@ -8,6 +8,6 @@ scanf ("%f",& V);
 printf ("nhap diem mon T= ");
 scanf ("%f",& T);
  printf(" (toan)T\t(van)V \n",V,T);
- printf (" nhap diem trung binh 2 mon :%.2f",V,T);
+ printf (" nhap diem trung binh 2 mon =%.2f",V,T);
  return 0;
 }
