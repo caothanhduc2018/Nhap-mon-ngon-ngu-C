20dfd25107b1c0b8754482c22b3f3644d21ca3ec
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2020-12-03 16:37:58 +0700

    Create ketquahoctap.c

---
+++ Bài tập 2/ketquahoctap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+double V,T;
+printf ("nhap diem mon V= ");
+scanf ("%f",& V);
+printf ("nhap diem mon T= ");
+scanf ("%f",& T);
+ printf(" (toan)\t(van)V \n" ,CV,DT);
+ printf (" nhap diem trung binh 3 mon :%.2f",V,T);
+ return 0;
+}
