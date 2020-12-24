f925d63530656b17cb1071f5d2aad1a5c3a40627
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2020-12-24 15:30:07 +0700

    Update lythuyet.c

--- Bài tập 3/lythuyet.c
+++ Bài tập 3/lythuyet.c
@@ -2,4 +2,20 @@ dem=dem+1
 * dem+=1
 * dem++
   
-  
+   #include <stdio.h>
+int main(){
+  int n, tong=0;
+  for ( int n = 1; n <= 10; n++){
+    tong=tong + n;}
+  printf("tong la: %d", tong);
+  tong = tong + 1;
+  tong = tong + 2;
+  tong = tong + 3;
+  tong = tong + 4;
+  tong = tong + 5;
+  tong = tong + 6;
+  tong = tong + 7;
+  tong = tong + 8;
+  tong = tong + 9;
+  tong = tong + 10;
+  return 0;
