881776daa6a38760a2f053a31eed7851fc73d6d9
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2020-12-28 21:42:43 +0700

    Create tamgiacso.c

---
+++ Bài tập 3/tamgiacso.c
@@ -0,0 +1,13 @@
+int main(){
+	int i,s,n;
+	
+	printf("Nhap n = ");
+	scanf("%d",&n);
+	
+	for (s = 1; s <= n; s++){
+		
+		for (i = 1; i <= s; i++){
+		printf("%d ",i);}
+		printf("\n");}
+	return 0;
+}
