b1d602e8baaf4da92bef7f00caad3cd1a238e7c2
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-04 21:26:39 +0700

    Update kt_02.c

--- Bài tập 3/kt_02.c
+++ Bài tập 3/kt_02.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
+
 int main(){
- // khai báo biến
-	float a, c, b, delta, x1, x2, x;
-	printf("Nhap vao he so phuong trinh bac hai: ");
-	scanf("%f%f%f", &a, &b, &c);
-	// xét duyệt các khả năng
-	if(a == 0) { 
-		if(b == 0 && c != 0) {
-			printf(" vo nghiem\n");
-		} else if(b == 0 && c == 0) {
-			printf(" co vo so nghiem\n");
-		} else {
-			x = -c / b;
-			printf("Nghiem phuong trinh = %0.2f", x);
-		}
-	} else { 
-		delta = b * b - a * c * 4; 
-		if(delta < 0) { // TH1
-			printf("vo nghiem");
-		} else if(delta == 0) { // TH2
-			x1 = -b/(2 * a);
-			// kết quả
-			printf("co nghiem kep x = %0.2f", x1);
-		} else { // TH3
-			x1 = (-b + sqrt(delta)) / (2 * a);
-			x2 = (-b - sqrt(delta)) / (2 * a);
-			// kết quả
-			printf("Nghiem PT la: \nx1 = %0.2f\nx2 = %0.2f", x1, x2);
-		}
-	}
+	float a, b, c,Denta;
 	
+	printf ("\n nhap vao a = ");
+	scanf ("%f", &a);
+	printf ("\n nhap vao b = ");
+	scanf ("%f", &b);
+	printf ("\n nhap vao c = ");
+	scanf ("%f", &c);
+ 
+	if (a == 0 && b==0 &&c==0){
+		printf ("\n phuong trinh co vo so nghiem");
+ 	}
+ 	else{
+		Denta = b * b - 4 * a * c ;
+		if (Denta < 0){
+  	 		printf ("\n phuong trinh vo nghiem ");
+ 		}
+		else if (Denta == 0 ){
+			float x = -b / (2 * a );
+   			printf ("\n phuong trinh co nghiem kep x1 = x2 = %f", x);
+ 		}
+		else { 
+    		float x1 = (-b + sqrt(Denta)) / (2 * a);
+			float x2 = (-b - sqrt(Denta)) / (2 * a);
+     		printf ("\nphuong trinh co 2 nghiem phan biet:\nx1 = %f\nx2 = %f" , x1 , x2);
+ 		}
+ 		}
+	getchar();
 	return 0;
-} 
+}
