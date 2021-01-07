95dbd398007cafbd2cedbba02ee9f485a0640256
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-07 16:11:55 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -2,7 +2,6 @@
 
 int main() {
   int a , b;
-  printf("bang nhan\n");
   a = 2, b = 1;
   printf ("%d x %d = %d\n", a, b , a*b);
   a = 2, b = 2;
@@ -10,7 +9,7 @@ int main() {
   a = 2, b = 3;
   printf ("%d x %d = %d\n", a, b , a*b);
 
-  printf ("bang nhan\n");
+  
   a = 3, b = 1;
   printf ("%d x %d = %d\n", a, b , a*b);
   a = 3, b = 2;
@@ -18,7 +17,7 @@ int main() {
   a = 3, b = 3;
   printf ("%d x %d = %d\n", a, b , a*b);
 
-  printf ("bang nhan\n");
+ 
   a = 4, b = 1;
   printf ("%d x %d = %d\n", a, b , a*b);
   a = 4, b = 2;
