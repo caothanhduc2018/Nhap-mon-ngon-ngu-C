74a888a8dec823048bdbe58d780c6404c49f2f0e
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-08 09:46:46 +0700

    Update kt_04.c

--- Bài tập 3/kt_04.c
+++ Bài tập 3/kt_04.c
@@ -13,3 +13,29 @@ int main(){
 
 return 0;
 }
+
+
+#include<stdio.h>
+int main(){   
+int n = 123;
+int s = 0; 
+s = s + n%10;
+n= n/10;
+
+s = s + n%10;
+n= n/10;
+
+s = s + n%10;
+n= n/10;
+ for (;n!=o; n%10)
+ s = s + n=n/10;
+
+ n=123
+ while (n!=0; n %10)
+ s = s + n=n/1=;
+
+printf ("\n tong cua so %d ");
+
+return 0 ; 
+}
+} 
