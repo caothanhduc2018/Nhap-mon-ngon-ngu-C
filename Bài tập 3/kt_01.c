967dfff8f938bbcc3e352e6b91636b44da20990d
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-08 08:55:28 +0700

    Update kt_01.c

--- Bài tập 3/kt_01.c
+++ Bài tập 3/kt_01.c
@@ -2,30 +2,11 @@
 
 int main() {
   int a , b;
-  printf ("bang nhan\n");
-  a = 2, b = 1;
-  printf ("%d x %d = %d\n", a, b , a*b);
-  a = 2, b = 2;
-  printf ("%d x %d = %d\n", a, b , a*b);
-  a = 2, b = 3;
-  printf ("%d x %d = %d\n", a, b , a*b);
-
-  printf ("bang nhan\n");
-  a = 3, b = 1;
-  printf ("%d x %d = %d\n", a, b , a*b);
-  a = 3, b = 2;
-  printf ("%d x %d = %d\n", a, b , a*b);
-  a = 3, b = 3;
-  printf ("%d x %d = %d\n", a, b , a*b);
-
-  printf("bang nhan\n");
-  a = 4, b = 1;
-  printf ("%d x %d = %d\n", a, b , a*b);
-  a = 4, b = 2;
-  printf ("%d x %d = %d\n", a, b , a*b);
-  a = 4, b = 3;
-  printf ("%d x %d = %d\n", a, b , a*b);
-
+  for (a= 1; a<=3; a++){
+    printf ("\n bang nhan: ");
+  for (b= 2; b<=3; b++){
+    
+    printf (" \n %d x %d = %d", a,b ,a*b);}}
   getchar();
   return 0 ;
 }
