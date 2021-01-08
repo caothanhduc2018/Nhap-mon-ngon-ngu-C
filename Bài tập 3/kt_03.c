7eb503fec0c72a9bd700aff276ea09bd3eb4b89c
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-08 10:48:12 +0700

    Update kt_03.c

--- Bài tập 3/kt_03.c
+++ Bài tập 3/kt_03.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
+#include <stdio.h>
 
 int main(){
 int i;
-for (i = 1; 1 <20; i+=2 )
-{
-if ((i == 5) || (i == 11) || (i == 15))
-continue;
-printf("%d\n", i);
+for (i = 1; i <20; i++){
+if (i%2 !=0){
+if(i == 5){  
+continue;}
+if(i == 11){
+continue;}
+if (i == 15 ){
+continue;}
+printf("%d\t", i);
+}}
+	getchar();
+	return 0;
 }
+
+
+#include <stdio.h>
+
+int main(){
+int i;
+while(i<20){
+  i++;
+if (i%2 !=0){
+if(i == 5){  
+continue;}
+if(i == 11){
+continue;}
+if (i == 15 ){
+continue;}
+printf("%d\t", i);
+
+}}
 	getchar();
 	return 0;
 }
