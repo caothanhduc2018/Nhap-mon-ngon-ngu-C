5c6982b9dc74bd91deb3be182a87d4bf08c7dffd
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-14 15:34:09 +0700

    Update mang_01.c

--- Bài tập 4/mang_01.c
+++ Bài tập 4/mang_01.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 int main(){
-int i, sum = 0;
-int a[4]={3, 5, 7, 2};
+int i;// khai bao 
+int sum = 0;// khai bao
+int a[4]={3, 5, 7, 2};// khai bao cac phan tu trong hop 
 for(i=0;i<4;i++){
-sum =sum + a[i]; 
-printf ("%d tong cac so phan tu: %d\n " ,i+0, a[i]);
-printf ("tong so %d\t", sum );
-  printf("%d\t",a[i]);
+sum =sum + a[i];// hien thi phan tu mang a[i] 
+printf (" phan tu  %d\t gia tri phan tu: %d\n " ,i+0, a[i]);
+// xuat ra gia tri cua cac mang da cho
+printf ("tong so %d\n", sum );
+// hien thi tong so theo tung phan tu 
+  printf("%d\n",a[i]);
+// hien thi phan tu trong mang  
 }
 
 return 0;
