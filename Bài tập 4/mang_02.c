992042a0525ec06dad9dc05e8e4d39cd52c56fff
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-19 14:32:58 +0700

    Update mang_02.c

--- Bài tập 4/mang_02.c
+++ Bài tập 4/mang_02.c
@@ -21,3 +21,30 @@ int a[n];
 
 return 0;
 }
+
+
+
+#include <stdio.h>
+
+
+int main(){
+  int n;
+  // yeu cau nguoi dung nhap n tu ban phim 
+  printf ("nhap n =  ");
+  scanf("%d", &n);
+  // tao mang a chua so nguyen kich thuoc n 
+  int a[n];
+  // nhap n phan tu cua mang a tu ban phim
+  for(int i = 0;  i < n; i++){
+  printf ("nhap phan tu thu %d  ", i);
+  scanf("%d", &a[i]);
+  printf("\n");
+  }
+  // hien thi cac phan tu cua mang a
+  printf("mang a:");
+    for(int i = 0;  i < n; i++){
+       if(a[i]%2==0)
+                 printf("\n%d",a[i]);}
+                     return 0;
+}
+  
