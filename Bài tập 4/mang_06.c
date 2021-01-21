de26ef1afa60c50bce6141057abdc7b83cab0bfb
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-21 16:06:21 +0700

    Create mang_06.c

---
+++ Bài tập 4/mang_06.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(){
+ int n;
+  // yeu cau nguoi dung nhap n tu ban phim 
+  printf ("nhap n =  ");
+  scanf("%d", &n);
+  // tao mang a chua so nguyen kich thuoc n 
+  int a[n];
+  // nhap n phan tu cua mang a tu ban phim
+  for(int i = 0;  i < n; i++){
+  printf ("a[%d]   ", i);
+  scanf("%d", &a[i]);
+  }
+   printf("mang a:");
+   for(int i = 0;  i < n; i++){
+  printf ("  %d ", a[i] );
+   }
+   int m;
+   printf (" \nm :");
+   scanf ("%d", &m);
+  int c = 0;
+  for ( int i = 0; i < n; i++){
+    if(a[i]  == m){
+      c = c + 1;
+    }}
+   printf (" \nso phan co gia tri là %d : %d" , m,c);
+   printf ("\nchi so cac phan tu ");
+   for (int i = 0; i < n; i++){
+     printf("  %d "   , i);
+   }
+ for (int i = 0; i < n; i++){
+    if(a[i] != m){
+    printf (" \nkhong ton tai %d", a[i]);
+
+  }}
+  
+
+return 0;
+  }
