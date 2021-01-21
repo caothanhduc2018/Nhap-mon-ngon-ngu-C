cdb71845534135c9110d928ffa57c0927dee40c7
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-21 14:31:10 +0700

    Create Bài tập mag c luu y.c

---
+++ Bài tập mag c luu y.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main(){
+  int dem_chan = 0;
+    int n;
+  // yeu cau nguoi dung nhap n tu ban phim 
+  printf ("nhap n =  ");
+  scanf("%d", &n);
+   n = 4;
+   int a[n];
+  a[0] = 1;
+  a[1] = 2;
+  a[2] = 3;
+  a[4] = 4;
+
+
+  if(a[0] % 2 == 0)
+  dem_chan = dem_chan + 1;
+   if(a[1] % 2 == 0)
+  dem_chan = dem_chan + 1;
+   if(a[2] % 2 == 0)
+  dem_chan = dem_chan + 1;
+     if(a[3] % 2 == 0)
+  dem_chan = dem_chan + 1;
+  printf ("cac so chan : %d" , dem_chan);
+  return 0;
+}
+
+
+int dem_chan = 0;
+int dem_le = 0;
+int dem_0 = 0;
+a[0] = 0;
+a[1] = 0;
+a[2] = 0
+for (int i = 0; i <= 2; i++){
+  if(a[i] % 2 == 0 && a[1] !=0)
+  dem_chan = dem_chan + 1
+
+  printf ("cac so chan: %d" ;dem_chan)
+  
