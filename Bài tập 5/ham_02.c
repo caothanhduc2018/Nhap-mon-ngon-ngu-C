101cbc0e9495028e6e43695750ad72168e8e4229
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-22 13:36:59 +0700

    Update ham_02.c

--- Bài tập 5/ham_02.c
+++ Bài tập 5/ham_02.c
@@ -16,34 +16,4 @@ int main(){
   return 0;
 }
 
-// khoi dinh nghia ham
-int kiem_tra_am_duong(int n){
-    if ((n) > 0 && (n) < 0){
 
-    }
-	return n;
-}
-
-
-//Khoi khai bao
-#include<stdio.h>
-int n, i nhap (int n, i );
-int nhap_pt_mang (int a[n]);
-int hien_thi_pt_mang ( );
-int hien_thi_tong_chan (int a[i], ac = 0);
-int dem_chan_le_0 (int a[i], )
-
- //khoi ham main
-int main(){
-  int n;
-  printf("Nhap n = ");
-  scanf("%d", &n);
-  int a[n];
-  Nhap_pt_mang(a);
-  Hien_thi_pt_mang(a);
-  Hien_thi_tong_chan(a);
-  Dem_chan_le_0(a);
-  return 0;
-}
-
-// khoi dinh nghia ham
