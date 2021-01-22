7b86ce46e0cf2cc21efb3f21c951d51dcab27c48
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-22 09:14:22 +0700

    Update ham_01.c

--- Bài tập 5/ham_01.c
+++ Bài tập 5/ham_01.c
@@ -7,3 +7,25 @@ int main(){
   printf ("a + b + c = %d",sum (3, 5, 2 ));
   return 0;
 }
+
+
+không có kiểu dữ liệu trả về
+// khoi khai bao 
+#include<stdio.h>
+void sum(int a, int b, int c);
+
+
+// khoi ham main
+int main(){
+int x = 1, y = 2, z = 3;
+sum(x , y ,z);
+
+
+  return 0; 
+}
+
+void sum(int a, int b , int c){
+  int d = a + b + c;
+ printf("tong la: %d", d);
+}
+// khoi dinh nghia ham 
