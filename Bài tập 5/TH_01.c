2459feb38b8ea93aabb06c81610c212339e7b467
Cao Thanh Đức <72543336+caothanhduc2018@users.noreply.github.com>
2021-01-23 09:30:01 +0700

    Create TH_01.c

---
+++ Bài tập 5/TH_01.c
@@ -0,0 +1,12 @@
+  
+#include <stdio.h>
+ int main(){
+   int n, tong = 0;
+   printf ("nhap so nguyen bat ky n = ");
+   scanf ("%d", &n);
+   for (int i = 0; i < n; i++){
+       tong = tong + i+1;
+   }
+     printf (" tong can tim %d ", tong);
+   return 0;
+   }
